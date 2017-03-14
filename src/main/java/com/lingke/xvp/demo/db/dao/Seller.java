3a984d1ae64a269a02f11edeed35519772dba913
gaoyihe <gaoyihe@rongcapital.cn>
2017-03-14 17:42:50 +0800

    用户注册、登录、重置密码功能添加

--- src/main/java/com/lingke/xvp/demo/db/dao/Seller.java
+++ src/main/java/com/lingke/xvp/demo/db/dao/Seller.java
@@ -1,5 +1,6 @@
 package com.lingke.xvp.demo.db.dao;
 
+import com.jfinal.plugin.activerecord.Db;
 import com.lingke.xvp.demo.db.dao.base.BaseSeller;
 
 /**
@@ -7,5 +8,29 @@ import com.lingke.xvp.demo.db.dao.base.BaseSeller;
  */
 @SuppressWarnings("serial")
 public class Seller extends BaseSeller<Seller> {
-	
+	public static final Seller dao = new Seller();
+
+	public static Seller addSeller(String phone, String password) throws Exception {
+		Seller seller = getSellerByPhone(phone);
+		if (seller != null) {
+			throw new RuntimeException("电话号码已经注册");
+		}
+		seller = new Seller();
+		seller.setPassword(password);
+		seller.setPhone(phone);
+		seller.save();
+		return seller;
+	}
+
+	public static Seller getSellerByPhone(String phone) {
+		return dao.findFirst("select * from seller where phone =?", phone);
+	}
+
+	public static Seller getSellerByPhoneAndPassword(String phone, String password) {
+		return dao.findFirst("select * from seller where phone =? and password=?", phone, password);
+	}
+
+	public static Boolean updateSellerPasswordByPhone(String phone, String password) {
+		return Db.update("update seller set password=? where phone =?", password, phone) > 0;
+	}
 }
