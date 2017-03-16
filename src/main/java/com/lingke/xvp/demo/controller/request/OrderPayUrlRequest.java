1f5b5c1be26fd23cb41f0dfb1d605981c9b1c750
gaoyihe <gaoyihe@rongcapital.cn>
2017-03-16 17:12:08 +0800

    手机验证码、获取收银台url、买家查询订单、买家查询订单详情、买家确认收货功能实现

---
+++ src/main/java/com/lingke/xvp/demo/controller/request/OrderPayUrlRequest.java
@@ -0,0 +1,9 @@
+package com.lingke.xvp.demo.controller.request;
+
+/**
+ * @author gaoyh
+ *
+ */
+public class OrderPayUrlRequest implements XvpRequest {
+
+}
---
+++ src/main/java/com/lingke/xvp/demo/controller/response/OrderPayUrlResponse.java
@@ -0,0 +1,27 @@
+package com.lingke.xvp.demo.controller.response;
+
+
+public class OrderPayUrlResponse implements XvpResponse {
+	/**
+	 * 收银台url
+	 */
+	private String url;
+
+	/**
+	 * @return the url
+	 */
+	public String getUrl() {
+		return url;
+	}
+
+	/**
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	
+	
+
+}
