958072f88f3dccf3cd69a1c941797ebed06907d9
yuweibin <yuweibin@rongcapital.cn>
2017-03-16 18:50:40 +0800

    代码格式化

--- src/main/java/com/lingke/xvp/demo/utils/SessionUtil.java
+++ src/main/java/com/lingke/xvp/demo/utils/SessionUtil.java
@@ -6,43 +6,54 @@ import org.springframework.web.context.request.RequestContextHolder;
 import com.lingke.xvp.demo.XvpConstants;
 
 public class SessionUtil {
-	public final static String SELLER_LOGIN_ID= "SELLER_LOGIN_ID";
-	public final static String USER_LOGIN_ID= "USER_LOGIN_ID";
-	public final static String SELLER_STORELOGIN_ID= "SELLER_STORELOGIN_ID";
-	public final static String USER_STORELOGIN_ID= "USER_STORELOGIN_ID";
-	public static void sellerLogin(Long value){
-		RequestContextHolder.getRequestAttributes().setAttribute(SELLER_LOGIN_ID, value, RequestAttributes.SCOPE_SESSION);
+	public final static String SELLER_LOGIN_ID = "SELLER_LOGIN_ID";
+	public final static String USER_LOGIN_ID = "USER_LOGIN_ID";
+	public final static String SELLER_STORELOGIN_ID = "SELLER_STORELOGIN_ID";
+	public final static String USER_STORELOGIN_ID = "USER_STORELOGIN_ID";
+
+	public static void sellerLogin(Long value) {
+		RequestContextHolder.getRequestAttributes().setAttribute(
+				SELLER_LOGIN_ID, value, RequestAttributes.SCOPE_SESSION);
 	}
-	public static boolean checkSellerLogin(){
-		return RequestContextHolder.getRequestAttributes().getAttribute(SELLER_LOGIN_ID, RequestAttributes.SCOPE_SESSION)!=null;	
+
+	public static boolean checkSellerLogin() {
+		return RequestContextHolder.getRequestAttributes().getAttribute(
+				SELLER_LOGIN_ID, RequestAttributes.SCOPE_SESSION) != null;
 	}
-	
-	public static void sellerSetStoreId(String value){
-		RequestContextHolder.getRequestAttributes().setAttribute(SELLER_STORELOGIN_ID, value, RequestAttributes.SCOPE_SESSION);
+
+	public static void sellerSetStoreId(String value) {
+		RequestContextHolder.getRequestAttributes().setAttribute(
+				SELLER_STORELOGIN_ID, value, RequestAttributes.SCOPE_SESSION);
 	}
-	public static String sellerGetStoreId(){
-		Object ob = RequestContextHolder.getRequestAttributes().getAttribute(SELLER_STORELOGIN_ID, RequestAttributes.SCOPE_SESSION);	
-		if(ob==null){
+
+	public static String sellerGetStoreId() {
+		Object ob = RequestContextHolder.getRequestAttributes().getAttribute(
+				SELLER_STORELOGIN_ID, RequestAttributes.SCOPE_SESSION);
+		if (ob == null) {
 			throw new RuntimeException(XvpConstants.ERROR_MESSAGE04);
 		}
 		return ob.toString();
 	}
-	
-	public static void userLogin(String value){
-		RequestContextHolder.getRequestAttributes().setAttribute(USER_LOGIN_ID, value, RequestAttributes.SCOPE_SESSION);
+
+	public static void userLogin(String value) {
+		RequestContextHolder.getRequestAttributes().setAttribute(USER_LOGIN_ID,
+				value, RequestAttributes.SCOPE_SESSION);
 	}
-	public static boolean checkUserLogin(){
-		return RequestContextHolder.getRequestAttributes().getAttribute(USER_LOGIN_ID, RequestAttributes.SCOPE_SESSION)!=null;	
+
+	public static boolean checkUserLogin() {
+		return RequestContextHolder.getRequestAttributes().getAttribute(
+				USER_LOGIN_ID, RequestAttributes.SCOPE_SESSION) != null;
 	}
-	
-	
-	
-	public static void userSetStoreId(String value){
-		RequestContextHolder.getRequestAttributes().setAttribute(USER_STORELOGIN_ID, value, RequestAttributes.SCOPE_SESSION);
+
+	public static void userSetStoreId(String value) {
+		RequestContextHolder.getRequestAttributes().setAttribute(
+				USER_STORELOGIN_ID, value, RequestAttributes.SCOPE_SESSION);
 	}
-	public static String userGetStoreId(){
-		Object ob = RequestContextHolder.getRequestAttributes().getAttribute(USER_STORELOGIN_ID, RequestAttributes.SCOPE_SESSION);	
-		if(ob==null){
+
+	public static String userGetStoreId() {
+		Object ob = RequestContextHolder.getRequestAttributes().getAttribute(
+				USER_STORELOGIN_ID, RequestAttributes.SCOPE_SESSION);
+		if (ob == null) {
 			throw new RuntimeException(XvpConstants.ERROR_MESSAGE04);
 		}
 		return ob.toString();
