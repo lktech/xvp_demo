80d36393b1c5e565547a710281b416fd92a3bd89
gaoyihe <gaoyihe@rongcapital.cn>
2017-03-16 10:17:15 +0800

    common解析提取到一个方法中修改

--- src/main/java/com/lingke/xvp/demo/controller/CommonController.java
+++ src/main/java/com/lingke/xvp/demo/controller/CommonController.java
@@ -1,12 +1,18 @@
 package com.lingke.xvp.demo.controller;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Rop.api.ApiException;
+import com.Rop.api.RopResponse;
 import com.Rop.api.domain.Area;
 import com.Rop.api.request.XvpCommonGetcityareasRequest;
 import com.Rop.api.request.XvpCommonGetcountyareasRequest;
@@ -39,17 +45,10 @@ public class CommonController {
 	 *             Api异常
 	 */
 	@RequestMapping(path = "/getprovinceareas", method = RequestMethod.POST)
-	public XvpResponse getprovinceareas(CommonRequest commonRequest) throws ApiException {
+	public XvpResponse getprovinceareas(@RequestBody CommonRequest commonRequest) throws ApiException {
 		XvpCommonGetprovinceareasRequest request = new XvpCommonGetprovinceareasRequest();
 		XvpCommonGetprovinceareasResponse ropResponse = ropClientAdapter.ropInvoke(request);
-		CommonListResponse listResponse = new CommonListResponse();
-		CommonResponse response = null;
-		for (Area area : ropResponse.getAreas()) {
-			response = new CommonResponse();
-			BeanUtils.copyProperties(area, response);
-			listResponse.add(response);
-		}
-		return listResponse;
+		return convertRopRespToXvpResp(ropResponse);
 	}
 
 	/**
@@ -62,18 +61,11 @@ public class CommonController {
 	 *             Api异常
 	 */
 	@RequestMapping(path = "/getcityareas", method = RequestMethod.POST)
-	public XvpResponse getcityareas(CommonRequest commonRequest) throws ApiException {
+	public XvpResponse getcityareas(@RequestBody CommonRequest commonRequest) throws ApiException {
 		XvpCommonGetcityareasRequest request = new XvpCommonGetcityareasRequest();
 		BeanUtils.copyProperties(commonRequest, request);
 		XvpCommonGetcityareasResponse ropResponse = ropClientAdapter.ropInvoke(request);
-		CommonListResponse listResponse = new CommonListResponse();
-		CommonResponse response = null;
-		for (Area area : ropResponse.getAreas()) {
-			response = new CommonResponse();
-			BeanUtils.copyProperties(area, response);
-			listResponse.add(response);
-		}
-		return listResponse;
+		return convertRopRespToXvpResp(ropResponse);
 	}
 
 	/**
@@ -86,13 +78,32 @@ public class CommonController {
 	 *             Api异常
 	 */
 	@RequestMapping(path = "/getcountyareas", method = RequestMethod.POST)
-	public XvpResponse getcountyareas(CommonRequest commonRequest) throws ApiException {
+	public XvpResponse getcountyareas(@RequestBody CommonRequest commonRequest) throws ApiException {
 		XvpCommonGetcountyareasRequest request = new XvpCommonGetcountyareasRequest();
 		BeanUtils.copyProperties(commonRequest, request);
 		XvpCommonGetcountyareasResponse ropResponse = ropClientAdapter.ropInvoke(request);
+		return convertRopRespToXvpResp(ropResponse);
+	}
+
+	/**
+	 * 从ropresp转换到ropresp
+	 * 
+	 * @param ropResponse
+	 *            rop返回结果
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	private CommonListResponse convertRopRespToXvpResp(RopResponse ropResponse) {
 		CommonListResponse listResponse = new CommonListResponse();
+		List<Area> areas = new ArrayList<>();
+		try {
+			areas = (List<Area>) ropResponse.getClass().getMethod("getAreas").invoke(ropResponse);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException
+				| SecurityException e) {
+			throw new RuntimeException("获取行政区划出错", e);
+		}
 		CommonResponse response = null;
-		for (Area area : ropResponse.getAreas()) {
+		for (Area area : areas) {
 			response = new CommonResponse();
 			BeanUtils.copyProperties(area, response);
 			listResponse.add(response);
