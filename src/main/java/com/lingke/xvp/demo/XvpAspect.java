9a9bdce2979b91a5cedbe88a14dc1a6aa94f75de
yuweibin <yuweibin@rongcapital.cn>
2017-03-17 13:42:22 +0800

    代码调整

--- src/main/java/com/lingke/xvp/demo/XvpAspect.java
+++ src/main/java/com/lingke/xvp/demo/XvpAspect.java
@@ -25,24 +25,28 @@ public class XvpAspect {
 	public Object doAround(ProceedingJoinPoint joinPoint) {
 		try {
 			String method = joinPoint.getSignature().toShortString();
-			logger.info("调用接口开始：{}，请求参数：{}", method,joinPoint.getArgs()!=null?
-					joinPoint.getArgs()[0].toString():null);
+			logger.info(
+					"调用接口开始：{}，请求参数：{}",
+					method,
+					joinPoint.getArgs().length > 0 ? joinPoint.getArgs()[0]
+							.toString() : null);
 			Object result = joinPoint.proceed();
-			logger.info("调用接口结束：{}，响应参数：{}", method, result==null?"":result.toString());
-			SuccessResponse successResponse  = new SuccessResponse();
+			logger.info("调用接口结束：{}，响应参数：{}", method, result == null ? ""
+					: result.toString());
+			SuccessResponse successResponse = new SuccessResponse();
 			successResponse.setCode(XvpConstants.SUCCESS);
-			if(result!=null){
+			if (result != null) {
 				successResponse.setResult(result);
 			}
 			return successResponse;
 		} catch (RuntimeException e) {
-			logger.error("请求处理返回运行时异常，errorMessage：{}", e.getMessage(),e);
+			logger.error("请求处理返回运行时异常，errorMessage：{}", e.getMessage(), e);
 			ExceptionResponse exceptionResponse = new ExceptionResponse();
 			exceptionResponse.setCode(XvpConstants.ERROR_CODE01);
 			exceptionResponse.setMessage(e.getMessage());
 			return exceptionResponse;
 		} catch (ApiException e) {
-			logger.error("请求处理返回Rop调用异常，errorMessage：{}", e.getMessage(),e);
+			logger.error("请求处理返回Rop调用异常，errorMessage：{}", e.getMessage(), e);
 			ExceptionResponse exceptionResponse = new ExceptionResponse();
 			exceptionResponse.setCode(XvpConstants.ERROR_CODE02);
 			exceptionResponse.setMessage(e.getMessage());
