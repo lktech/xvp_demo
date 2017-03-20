e43085187ba6c9de431a80375285ef381828eaa5
gaoyihe <gaoyihe@rongcapital.cn>
2017-03-20 11:01:05 +0800

    增加初始化编码连接器

--- src/main/java/com/lingke/xvp/demo/XvpApp.java
+++ src/main/java/com/lingke/xvp/demo/XvpApp.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.filter.OrderedCharacterEncodingFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 /**
  * Created by yuwb on 2017-3-13.
@@ -17,5 +20,17 @@ public class XvpApp {
 		SpringApplication.run(XvpApp.class, args);
 		loogger.info("#######################  APP Success Started . ################################");
 	}
-
+    /**
+     * 初始化编码连接器
+     * @return
+     */
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        loogger.info("#######################  Init CharacterEncodingFilter ... ################################");
+        CharacterEncodingFilter filter = new OrderedCharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceRequestEncoding(true);
+        filter.setForceResponseEncoding(true);
+        return filter;
+    }
 }
