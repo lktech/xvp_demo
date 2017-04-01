1408375f6c559c8de282f69a2f1409459d8adc17
wangqi <wangqi@rongcapital.cn>
2017-04-01 11:32:29 +0800

    3

--- web/mall/src/views/product/detail.vue
+++ web/mall/src/views/product/detail.vue
@@ -42,7 +42,7 @@
                 goods:{},//商品信息
                 skuList:[],//商品sku列表
                 skuList1:[],
-                storeInfo:{name:'撒娇的科技实力卡家肯德基卡拉斯京的'},//店铺信息
+                storeInfo:{name:localStorage.getItem('store_name')},//店铺信息
                 content:{},//商品详细信息
                 goods_id:0,//商品id
                 sku:{
