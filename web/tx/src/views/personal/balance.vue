46b796b8635feb1fe0181bc160626c93e5d51e56
zhaoze <zhaoze@rongcapital.cn>
2017-05-04 16:15:48 +0800

    更新项目环境

--- web/tx/src/views/personal/balance.vue
+++ web/tx/src/views/personal/balance.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="balance">
         <c-top-back></c-top-back>
-        <r-messages :status="1" msg="我的账户余额" :money="money" :desc="desc">
-            <div slot="list" class="slot" v-if="show">
+        <r-messages :status="1" msg="我的账户余额" :money="money" :desc="desc" class="account">
+            <div slot="list" class="slot">
                 <r-group>
-                    <r-cell title="支付方式" :value="payWay"></r-cell>
-                    <!--<r-cell title="关注妈妈公开课公众号" @click.native="openBtn" is-link></r-cell>-->
+                    <r-cell title="提现账号" value="未实名验证"></r-cell>
+                    <r-cell title="提现记录" is-link></r-cell>
                 </r-group>
             </div>
             <div slot="btn">
-                <r-button type="primary" :text="btnText" @click.native="btnClick"></r-button>
+                <r-button type="primary" text="提现" @click.native="btnClick"></r-button>
             </div>
         </r-messages>
     </div>
@@ -20,14 +20,19 @@
     export default {
         data (){
             return {
-                money:1000
+                money:1000,
+                desc:"提现预计24小时后到账",
             }
         },
 
-        methods: {},
+        methods: {
+            btnClick(){
+
+            }
+        },
         components: {
             "cTopBack": require('../../components/x-top-back/x-top-back.vue'),
-            "rPopup": require("../../components/popup/popup.vue"),
+            "rGroup": require("../../components/group/group.vue"),
             "rButton": require("../../components/button/button.vue"),
             "rCell": require("../../components/cell/cell.vue"),
             "r-messages": require("../../components/x-messages/x-messages.vue"),
