e93ffe120d026b66e5e4f65bc0f7bc5354577408
zhaoze <zhaoze@rongcapital.cn>
2017-05-08 17:45:36 +0800

    项目迁移

---
+++ web/seller/mockapi-zz.js
@@ -0,0 +1,76 @@
+Mock.mock(basepath + "/seller/account/getAccountAmount", "post", {
+    code: "SUCCESS",
+    result: {
+        withdrawals_amount: 10000,
+        today_income_amount: 100000,
+        income_amount: 100000000000
+    }
+});
+
+Mock.mock(basepath + "/seller/seller/verify", "post", {code:"SUCCESS",result:{sn:1}});
+Mock.mock(basepath + "/1", "post", {
+    code: "SUCCESS",
+    result: {}
+});
+Mock.mock(basepath + "/1", "post", {
+    code: "SUCCESS",
+    result: {
+        "account_type": "1",
+        "company_name": "融数金服",
+        "buslince": "110105019962316",
+        "buslince_pic": "{\"buslince_pic\": \"http://img4.xiaovpu.com/71.png\"}",
+        "user_name": "张三",
+        "certificate_type": "0",
+        "certificate_number": "220102198706019690",
+        "createTime": "2017-02-09 12:55:52"
+    }
+});
+Mock.mock(basepath + "/seller/account/getBankInfo", "post", {
+    code: "SUCCESS",
+    result:
+        [
+            {
+                "bankcode": "123456789",
+                "bankname": "招商银行"
+            }
+        ]
+});
+Mock.mock(basepath + "/seller/account/getBankCity", "post", {
+    code: "SUCCESS",
+    result:
+        [
+            {
+                "code": "220100",
+                "name": "石家庄市"
+            },
+            {
+                "code": "220200",
+                "name": "廊坊市"
+            }
+        ]
+
+});
+Mock.mock(basepath + "/1", "post", {
+    code: "SUCCESS",
+    result: {}
+});
+Mock.mock(basepath + "/1", "post", {
+    code: "SUCCESS",
+    result: {}
+});
+Mock.mock(basepath + "/1", "post", {
+    code: "SUCCESS",
+    result: {}
+});
+Mock.mock(basepath + "/1", "post", {
+    code: "SUCCESS",
+    result: {}
+});
+Mock.mock(basepath + "/1", "post", {
+    code: "SUCCESS",
+    result: {}
+});
+Mock.mock(basepath + "/1", "post", {
+    code: "SUCCESS",
+    result: {}
+});
