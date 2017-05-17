13cb01a0e67912ad3449fbe882c2432981315c6f
zhaoze <zhaoze@rongcapital.cn>
2017-05-17 15:00:53 +0800

    微信配置

--- web/seller/src/libs/utils.js
+++ web/seller/src/libs/utils.js
@@ -638,9 +638,9 @@ exports.MenuShare = function () {
             if (data.code == "SUCCESS") {
                 wx.config({
                     debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                    appId: data.result.appId, // 必填，公众号的唯一标识
+                    appId: data.result.appid, // 必填，公众号的唯一标识
                     timestamp: data.result.timestamp, // 必填，生成签名的时间戳
-                    nonceStr: data.result.nonceStr, // 必填，生成签名的随机串
+                    nonceStr: data.result.noncestr, // 必填，生成签名的随机串
                     signature: data.result.signature,// 必填，签名，见附录1
                     jsApiList: ['onMenuShareAppMessage', 'onMenuShareTimeline', 'hideMenuItems'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
                 });
